4cdd7ab3add2563f8c43c70d9a9c3bd7bb4b8727
Ety-Biswangri <etybgc02@gmail.com>
2022-05-23 18:41:39 +0600

    home page item parts implemented

--- src/components/Home/PartsItem/PartItem.js
+++ src/components/Home/PartsItem/PartItem.js
@@ -10,13 +10,26 @@ const PartItem = ({ part }) => {
                 <img src={image} alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">{name}</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel molestias officiis, sapiente omnis saepe laboriosam libero, qui, officia quas reiciendis quam hic. Odit consequuntur amet deserunt et unde id corporis!</p>
-                <p>Minimum Order Quantity: {minOrderQuantity}</p>
-                <p>Available Quantity: {availableQuantiy}</p>
-                <p>Price (per unit): ${price}</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                <h2 class="card-title font-bold mb-3">{name}</h2>
+
+                <p className='text-justify mb-3'>{description}</p>
+
+                <p>
+                    <span className='font-bold text-base'> Minimum Order Quantity: </span> <span> {minOrderQuantity} </span>
+                </p>
+
+                <p>
+                    <span className='font-bold'> Available Quantity: </span>
+                    <span> {availableQuantiy} </span>
+                </p>
+
+                <p>
+                    <span className='font-bold'> Price (per unit): </span>
+                    <span className='text-lg'> ${price} </span>
+                </p>
+
+                <div class="card-actions mt-2">
+                    <button class="btn btn-primary text-white">Purchase Now</button>
                 </div>
             </div>
         </div >
