e7d0a9db7365f7e02691fc6febbeb9e400e88a60
Ety-Biswangri <etybgc02@gmail.com>
2022-05-24 00:31:02 +0600

    footer done

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='flex justify-center items-center h-36 bg-accent mt-10 text-white 2xl:max-w-7xl mx-auto'>
-            <div className='text-center'>
-                <p>All rights reserved &copy; {new Date().getFullYear()}</p>
-                <p><small>Visit Me on</small></p>
+        <footer class="footer p-10 bg-accent text-neutral-content">
+            <div className='footer flex flex-col md:flex-row md:justify-around md:items-center'>
+                <div className='mb-5 lg:mb-0'>
+                    <span class="footer-title">Services</span>
+                    <a class="link link-hover">Branding</a>
+                    <a class="link link-hover">Design</a>
+                    <a class="link link-hover">Marketing</a>
+                    <a class="link link-hover">Advertisement</a>
+                </div>
+                <div className='mb-5 lg:mb-0'>
+                    <span class="footer-title">Company</span>
+                    <a class="link link-hover">About us</a>
+                    <a class="link link-hover">Contact</a>
+                    <a class="link link-hover">Jobs</a>
+                    <a class="link link-hover">Press kit</a>
+                </div>
+                <div className='mb-5 lg:mb-0'>
+                    <span class="footer-title">Legal</span>
+                    <a class="link link-hover">Terms of use</a>
+                    <a class="link link-hover">Privacy policy</a>
+                    <a class="link link-hover">Cookie policy</a>
+                </div>
             </div>
-        </div>
+        </footer>
     );
 };
 
