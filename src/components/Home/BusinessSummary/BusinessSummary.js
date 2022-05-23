4f35579f25a0a8ebd0616da7d85625f0cc19c5f5
Ety-Biswangri <etybgc02@gmail.com>
2022-05-23 22:55:12 +0600

    business summary done

--- src/components/Home/BusinessSummary/BusinessSummary.js
+++ src/components/Home/BusinessSummary/BusinessSummary.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { MdTask } from 'react-icons/md';
+import { BiHappy } from 'react-icons/bi';
+import { MdOutlineRateReview } from 'react-icons/md';
+import { BsFillFlagFill } from 'react-icons/bs';
 
 const BusinessSummary = () => {
     return (
@@ -11,39 +15,31 @@ const BusinessSummary = () => {
 
                     <div class="stat">
                         <div class="stat-figure text-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                            <BsFillFlagFill className='text-4xl'></BsFillFlagFill>
                         </div>
-                        <div class="stat-value text-primary">50</div>
+                        <div class="stat-value text-secondary">50</div>
                         <div class="stat-title font-semibold">Countries</div>
                     </div>
 
                     <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <div class="avatar online">
-                                <div class="w-16 rounded-full">
-                                    <img src="https://api.lorem.space/image/face?w=128&h=128" />
-                                </div>
-                            </div>
+                        <div class="stat-figure text-primary">
+                            <MdTask className='text-4xl'></MdTask>
                         </div>
                         <div class="stat-value text-secondary">550</div>
                         <div class="stat-title font-semibold">Completed Tasks</div>
                     </div>
 
                     <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                        <div class="stat-figure text-primary">
+                            <BiHappy className='text-4xl'></BiHappy>
                         </div>
                         <div class="stat-value text-secondary">300</div>
                         <div class="stat-title font-semibold">Happy Customers</div>
                     </div>
 
                     <div class="stat">
-                        <div class="stat-figure text-secondary">
-                            <div class="avatar online">
-                                <div class="w-16 rounded-full">
-                                    <img src="https://api.lorem.space/image/face?w=128&h=128" />
-                                </div>
-                            </div>
+                        <div class="stat-figure text-primary">
+                            <MdOutlineRateReview className='text-4xl'></MdOutlineRateReview>
                         </div>
                         <div class="stat-value text-secondary">450</div>
                         <div class="stat-title font-semibold">Reviews</div>
