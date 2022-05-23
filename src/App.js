d9da542149830709b576f0d65609f62ec27997ef
Ety-Biswangri <etybgc02@gmail.com>
2022-05-23 13:27:02 +0600

    header and footer part is initialized

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Header from './components/Home/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/Home/HomePage/HomePage';
 import Footer from './components/Home/Footer/Footer';
+import Dashboard from './components/Dashboard/Dashboard';
+import LoginPage from './components/Login/LoginPage/LoginPage';
+import Register from './components/Login/Register/Register';
+import MyPortfolio from './components/MyPortfolio/MyPortfolio';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Routes>
-        <Route path='/' element={<HomePage></HomePage>}></Route>
-        <Route path='/home' element={<HomePage></HomePage>}></Route>
-      </Routes>
+      <div className='max-w-7xl mx-auto'>
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>}></Route>
+          <Route path='/home' element={<HomePage></HomePage>}></Route>
+          <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+          <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+          <Route path='/register' element={<Register></Register>}></Route>
+          <Route path='/portfolio' element={<MyPortfolio></MyPortfolio>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        </Routes>
+      </div>
       <Footer></Footer>
     </div>
   );
--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
+        <div className='flex justify-center items-center h-36 bg-accent my-10 text-white'>
+            <div className='text-center'>
+                <p>All rights reserved &copy; {new Date().getFullYear()}</p>
+                <p><small>Visit Me on</small></p>
+            </div>
         </div>
     );
 };
